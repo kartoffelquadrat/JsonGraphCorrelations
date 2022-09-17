227e3189e9a5ea75898a786264d2cf0071984b49
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-09-17 18:37:00 -0400

    removed gz file

--- docs/graphNavigationUtils.js
+++ docs/graphNavigationUtils.js
@@ -1,6 +1,13 @@
+// Global variable that all functions cn access anytime, e.g. to recenter on root node without reloading entire graph from disk.
+let graph_map
+let root_node
+
 function registerElementListeners() {
     document.getElementById('children-tab').addEventListener("click", () => toggleTabs("parents"));
     document.getElementById('parents-tab').addEventListener("click", () => toggleTabs("children"));
+    document.getElementById('focus-root').addEventListener("click", () => focusNodeById(root_node));
+    document.getElementById('switchExtensions').addEventListener("click", () => toggleExtensionLines());
+    document.getElementById('switchIdentifiers').addEventListener("click", () => toggleIdentifierLines());
 }
 
 function toggleTabs(target) {
@@ -12,6 +19,14 @@ function toggleTabs(target) {
         document.getElementById('children-tab').classList.add("active")
         document.getElementById('parents-tab').classList.remove("active")
     }
+}
+
+function toggleIdentifierLines() {
+    alert("IDs")
+}
+
+function toggleExtensionLines() {
+    alert("Exts")
 }
 
 // Iterates over map and increases counter for every entry that cas no children.
@@ -36,28 +51,36 @@ function findRootNote(graphMap) {
 }
 
 // updates the static information field for the current node
-function focusNodeById(node_key, node_values) {
+function focusNodeById(node_key) {
 
+    console.log(graph_map)
+    // Update current node panel
     document.getElementById("current-id").innerText = node_key
-    document.getElementById("current-extensions").innerText = node_values.ext
-    document.getElementById("current-payload").innerText = node_values.tpl
+    document.getElementById("current-extensions").innerText = graph_map.get("2").ext
+    let node = graph_map.get(node_key)
+    if (node.tpl === "")
+        document.getElementById("current-payload").innerText = "- NO PAYLOAD -"
+    else
+        document.getElementById("current-payload").innerText = graph_map.get("2").tpl
+
+    // update specialization / generalization panel
+    // ...
 }
 
 function initializeBoard(graph) {
     console.log(graph)
 
     // Convert object to map
-    let graph_map = new Map(Object.entries(graph))
+    graph_map = new Map(Object.entries(graph))
 
     // Update stats
-    const root_node = findRootNote(graph_map)
+    root_node = findRootNote(graph_map)
     document.getElementById("stats-total").innerText = graph_map.size
     document.getElementById("stats-leaves").innerText = countGraphLeaves(graph_map)
     document.getElementById("stats-root").innerText = root_node
 
     // Focus root node
-    focusNodeById(root_node, graph_map.get("1"))
-
+    focusNodeById(root_node, graph_map.get(root_node))
 }
 
 function loadGraph() {
