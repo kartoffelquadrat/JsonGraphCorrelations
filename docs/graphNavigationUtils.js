52037aad17a78603e0d7277177d5bfd62fa4e0c5
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-09-17 17:49:08 -0400

    enables JSON graph loading

--- docs/graphNavigationUtils.js
+++ docs/graphNavigationUtils.js
@@ -5,15 +5,31 @@ function registerElementListeners() {
 
 function toggleTabs(target) {
     console.log("switching to: " + target)
-    if(target === "children") {
+    if (target === "children") {
         document.getElementById('children-tab').classList.remove("active")
         document.getElementById('parents-tab').classList.add("active")
-    }
-    else
-    {
+    } else {
         document.getElementById('children-tab').classList.add("active")
         document.getElementById('parents-tab').classList.remove("active")
-
     }
+}
+
+
+function logGraph(graph) {
+    console.log(graph)
+
+    // Convert object to map
+    let graphMap = new Map(Object.entries(graph))
+
+    // Update stats
+    document.getElementById("stats-total").innerText = graphMap.size
+    document.getElementById("stats-leaves").innerText = "100"
+}
+
+function loadGraph() {
+
+    fetch("minified_lattice.json")
+        .then(result => result.json())
+        .then(graph => logGraph(graph))
 }
 
