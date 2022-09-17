9cc2480e5678a4964ba12b232eef83db80a95e9d
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-09-17 16:19:34 -0400

    replaces bootstrap js by native functions

--- docs/graphNavigationUtils.js
+++ docs/graphNavigationUtils.js
@@ -1,3 +1,19 @@
-function boo() {
-    console.log("JS File loaded.")
+function registerElementListeners() {
+    document.getElementById('children-tab').addEventListener("click", () => toggleTabs("parents"));
+    document.getElementById('parents-tab').addEventListener("click", () => toggleTabs("children"));
 }
+
+function toggleTabs(target) {
+    console.log("switching to: " + target)
+    if(target === "children") {
+        document.getElementById('children-tab').classList.remove("active")
+        document.getElementById('parents-tab').classList.add("active")
+    }
+    else
+    {
+        document.getElementById('children-tab').classList.add("active")
+        document.getElementById('parents-tab').classList.remove("active")
+
+    }
+}
+
