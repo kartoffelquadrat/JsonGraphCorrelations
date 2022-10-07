22369e138a8b4eb935dad3b00ff8b6c8e6edf66f
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-10-07 08:55:15 -0400

    Added slider (dummy)

--- docs/uiElements.js
+++ docs/uiElements.js
@@ -1,3 +1,16 @@
+function buildSliderFilter() {
+    let slider = document.getElementById('slider');
+
+    noUiSlider.create(slider, {
+        start: [20, 80],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 100
+        }
+    });
+}
+
 function registerElementListeners() {
     document.getElementById('children-tab').addEventListener("click", () => toggleTabs("children"));
     document.getElementById('parents-tab').addEventListener("click", () => toggleTabs("parents"));
